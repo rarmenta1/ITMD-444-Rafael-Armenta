0dd69f1abdc4f17ce10a6355e0c1603e28c0582f
Rafael Armenta <rarmenta@hawk.iit.edu>
2024-02-29 07:03:09 -0600

    renamed connection string

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MongoDB_Connection_String;
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
